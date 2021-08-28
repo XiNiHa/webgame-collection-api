ca4882c85bc434fd21142dee4fe48ff3af4ae6e1
XiNiHa <anesin1109@naver.com>
2021-08-28 14:39:01 +0900

    games 쿼리 Cursor Pagination 적용

--- src/schema/query/game.rs
+++ src/schema/query/game.rs
@@ -1,38 +1,89 @@
-use std::iter::FromIterator;
-
-use async_graphql::*;
+use async_graphql::{connection::*, *};
+use chrono::{DateTime, Utc};
 use sqlx::PgPool;
 
-use crate::schema::types::{game::Game, localized_string::LocalizedString};
+use crate::schema::types::{
+    game::Game, localized_string::LocalizedString, scalars::DateTimeScalar,
+};
 
 #[derive(Default)]
 pub struct GameQuery;
 
 #[Object]
 impl GameQuery {
-    async fn games(&self, ctx: &Context<'_>) -> Result<Vec<Game>> {
+    async fn games(
+        &self,
+        ctx: &Context<'_>,
+        after: Option<String>,
+        before: Option<String>,
+        first: Option<i32>,
+        last: Option<i32>,
+    ) -> Result<Connection<DateTimeScalar, Game>> {
         let pool = ctx.data::<PgPool>()?;
 
-        let rows = sqlx::query!(
-            r#"
-            SELECT
-                id,
-                name AS "name: LocalizedString",
-                min_players,
-                max_players,
-                description AS "description: LocalizedString"
-            FROM public.game
-            "#
-        )
-        .fetch_all(pool)
-        .await?;
+        query(
+            after,
+            before,
+            first,
+            last,
+            |after: Option<DateTimeScalar>, before, first, last| async move {
+                let after = after.map(|scalar| scalar.0);
+                let before = before.map(|scalar| scalar.0);
 
-        Ok(Vec::from_iter(rows.into_iter().map(|row| Game {
-            id: row.id,
-            name: row.name,
-            min_players: row.min_players,
-            max_players: row.max_players,
-            description: row.description,
-        })))
+                let rows = sqlx::query!(
+                    r#"
+                    SELECT
+                        id,
+                        name AS "name: LocalizedString",
+                        min_players,
+                        max_players,
+                        description AS "description: LocalizedString",
+                        created_at,
+                        after AS "after: DateTime<Utc>",
+                        before AS "before: DateTime<Utc>"
+                    FROM
+                        public.game,
+                        (VALUES ($1::TIMESTAMPTZ, $2::TIMESTAMPTZ)) s(after, before)
+                    WHERE
+                        (after IS NULL OR created_at > after) AND
+                        (before IS NULL OR created_at < before)
+                    ORDER BY
+                        (CASE WHEN $3 THEN created_at END) DESC,
+                        created_at ASC
+                    LIMIT $4 + 1
+                    "#,
+                    after,
+                    before,
+                    last.is_some(),
+                    first.or(last).unwrap_or(10) as i32,
+                )
+                .fetch_all(pool)
+                .await?;
+
+                let mut connection = Connection::new(
+                    first.is_none() && rows.len() > last.unwrap_or(10),
+                    last.is_none() && rows.len() > first.unwrap_or(10),
+                );
+                let iter = rows.into_iter().map(|row| {
+                    Edge::new(
+                        DateTimeScalar(row.created_at),
+                        Game {
+                            id: row.id,
+                            name: row.name,
+                            min_players: row.min_players,
+                            max_players: row.max_players,
+                            description: row.description,
+                        },
+                    )
+                });
+                if last.is_some() {
+                    connection.append(iter.take(last.unwrap_or(10)).rev());
+                } else {
+                    connection.append(iter.take(first.unwrap_or(10)));
+                }
+                Ok(connection)
+            },
+        )
+        .await
     }
 }
--- src/schema/types/scalars.rs
+++ src/schema/types/scalars.rs
@@ -1,4 +1,6 @@
-use async_graphql::*;
+use std::{fmt::Display, str};
+
+use async_graphql::{connection::CursorType, *};
 use chrono::{DateTime, Utc};
 use sqlx::types::Uuid;
 
@@ -35,5 +37,36 @@ impl ScalarType for DateTimeScalar {
 
     fn to_value(&self) -> Value {
         Value::String(self.0.to_rfc3339())
+    }
+}
+
+impl CursorType for DateTimeScalar {
+    type Error = DateTimeCursorDecodeError;
+
+    fn decode_cursor(s: &str) -> Result<Self, Self::Error> {
+        let vec = base64::decode(s).map_err(DateTimeCursorDecodeError::Base64)?;
+        let s = str::from_utf8(vec.as_slice()).map_err(DateTimeCursorDecodeError::Utf8)?;
+        Ok(DateTimeScalar(
+            DateTime::parse_from_rfc3339(s)
+                .map_err(DateTimeCursorDecodeError::DateTime)?
+                .with_timezone(&Utc),
+        ))
+    }
+
+    fn encode_cursor(&self) -> String {
+        base64::encode(self.0.to_rfc3339())
+    }
+}
+
+#[derive(Debug)]
+pub enum DateTimeCursorDecodeError {
+    Base64(base64::DecodeError),
+    Utf8(str::Utf8Error),
+    DateTime(chrono::ParseError),
+}
+
+impl Display for DateTimeCursorDecodeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("{:?}", self))
     }
 }
