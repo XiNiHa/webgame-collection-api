9ce7c95fef681e95e055e9b61b1a0dca0f53e3c6
XiNiHa <anesin1109@naver.com>
2021-08-28 11:04:33 +0900

    config crate를 사용하여 dotenv 설정 로딩

--- src/config.rs
+++ src/config.rs
@@ -1,56 +1,54 @@
-use std::{env, num::NonZeroU32};
+use std::{fmt, num::NonZeroU32};
 
+use config::{Config, ConfigError, Environment};
+use serde::{de, Deserialize};
+
+#[derive(Debug, Deserialize)]
 pub struct AppConfig {
     pub database_url: String,
     pub pbkdf2_salt_size: usize,
     pub pbkdf2_iterations: NonZeroU32,
+    #[serde(deserialize_with = "deserialize_base64_string")]
     pub jwt_secret: Vec<u8>,
     pub refresh_token_size: usize,
+}
+
+impl AppConfig {
+    pub fn from_env() -> Result<Self, ConfigError> {
+        let mut cfg = Config::new();
+        cfg.merge(Environment::new())?;
+        cfg.try_into()
+    }
+}
+
+fn deserialize_base64_string<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    struct Base64StringVisitor;
+
+    impl<'de> de::Visitor<'de> for Base64StringVisitor {
+        type Value = Vec<u8>;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a base64 encoded string")
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            base64::decode(v).map_err(E::custom)
+        }
+    }
+
+    deserializer.deserialize_any(Base64StringVisitor)
 }
 
 lazy_static::lazy_static! {
     pub static ref CONFIG: AppConfig = {
         dotenv::dotenv().ok();
 
-        let database_url = match env::var("DATABASE_URL") {
-            Ok(var) => var,
-            Err(_) => panic!("Environment variable \"DATABASE_URL\" not set"),
-        };
-        let pbkdf2_salt_size = match env::var("PBKDF2_SALT_SIZE") {
-            Ok(var) => match var.parse::<usize>() {
-                Ok(size) => size,
-                Err(_) => panic!("Failed to parse environment variable \"PBKDF2_SALT_SIZE\""),
-            },
-            Err(_) => panic!("Environment variable \"PBKDF2_SALT_SIZE\" not set"),
-        };
-        let pbkdf2_iterations = match env::var("PBKDF2_ITERATIONS") {
-            Ok(var) => match var.parse::<NonZeroU32>() {
-                Ok(iterations) => iterations,
-                Err(_) => panic!("Failed to parse environment variable \"PBKDF2_ITERATIONS\""),
-            },
-            Err(_) => panic!("Environment variable \"PBKDF2_ITERATIONS\" not set"),
-        };
-        let jwt_secret = match env::var("JWT_SECRET") {
-            Ok(var) => match base64::decode(var) {
-                Ok(decoded) => decoded,
-                Err(_) => panic!("Failed to parse environment variable \"JWT_SECRET\" which should be a valid base64 string")
-            },
-            Err(_) => panic!("Environment variable \"JWT_SECRET\" not set"),
-        };
-        let refresh_token_size = match env::var("REFRESH_TOKEN_SIZE") {
-            Ok(var) => match var.parse::<usize>() {
-                Ok(size) => size,
-                Err(_) => panic!("Failed to parse environment variable \"REFRESH_TOKEN_SIZE\""),
-            },
-            Err(_) => panic!("Environment variable \"REFRESH_TOKEN_SIZE\" not set"),
-        };
-
-        AppConfig {
-            database_url,
-            pbkdf2_salt_size,
-            pbkdf2_iterations,
-            jwt_secret,
-            refresh_token_size,
-        }
+        AppConfig::from_env().unwrap()
     };
 }
